af18ab86bb50b594e419d150789368dd7acbd177
pkalakandan <pralanthan.kalakandan@pexa.com.au>
2022-02-06 16:50:53 +1100

    first poc

--- src/main/java/com/example/origincodesample/controller/PaymentsController.java
+++ src/main/java/com/example/origincodesample/controller/PaymentsController.java
@@ -4,10 +4,12 @@ import com.pk.origin.api.PaymentApi;
 import com.pk.origin.models.Payment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PaymentsController implements PaymentApi {
     @Override
     public ResponseEntity<Payment> payment(Payment body) {
-        return null;
+        return new ResponseEntity<>(body, HttpStatus.OK);
     }
 }
