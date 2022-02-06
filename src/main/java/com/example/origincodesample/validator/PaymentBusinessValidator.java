01fde84f2490a7a86ab21f93592f1e44c806ecaa
pkalakandan <pralanthan.kalakandan@pexa.com.au>
2022-02-06 22:09:13 +1100

    Added Exception handler and Validators

--- src/main/java/com/example/origincodesample/validator/PaymentBusinessValidator.java
+++ src/main/java/com/example/origincodesample/validator/PaymentBusinessValidator.java
@@ -1,8 +1,15 @@
 package com.example.origincodesample.validator;
 
 import com.example.origincodesample.dto.PaymentDto;
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+import static java.time.DayOfWeek.SUNDAY;
+
 
 @Component
 public class PaymentBusinessValidator implements org.springframework.validation.Validator {
@@ -19,6 +26,38 @@ public class PaymentBusinessValidator implements org.springframework.validation.
             errors.reject("100");
             return;
         }
+
+        LocalDateTime currentLocalDateTime = LocalDateTime.now();
+        final DayOfWeek dayOfWeek = currentLocalDateTime.getDayOfWeek();
+        switch (dayOfWeek) {
+            case MONDAY:
+            case TUESDAY:
+            case WEDNESDAY:
+            case THURSDAY:
+                if(currentLocalDateTime.getHour() < 9 || currentLocalDateTime.getHour() > 17) {
+                    errors.reject("400");
+                }
+                break;
+            case FRIDAY:
+                if(currentLocalDateTime.getHour() < 8 || currentLocalDateTime.getHour() > 18) {
+                    errors.reject("400");
+                }
+                break;
+            case SATURDAY:
+            //case SUNDAY:
+                errors.reject("200");
+                break;
+
+        }
+//        if (dayOfWeek.equals(SUNDAY) ||
+//                dayOfWeek.equals(DayOfWeek.SATURDAY)){
+//            errors.reject("200");
+//            return;
+//        } else {
+//
+//        if (dayOfWeek.equals()){
+//
+//        }
 
     }
 }
